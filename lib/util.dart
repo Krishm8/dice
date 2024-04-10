d2afb515afffb8d0cfb4e6342f2a466e2a32b630
Krishm8 <krishmakadia7@gmail.com>
2024-04-10 10:15:46 +0530

    Initial commit

---
+++ lib/util.dart
@@ -0,0 +1,8 @@
+List dice=[
+  {"image": "images/1.png", "value": 1},
+  {"image": "images/2.png", "value": 2},
+  {"image": "images/3.png", "value": 3},
+  {"image": "images/4.png", "value": 4},
+  {"image": "images/5.png", "value": 5},
+  {"image": "images/6.png", "value": 6},
+];
